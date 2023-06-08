f61fa1786034073fbbcc93ed8670673f0ab62e16
unknown <mandlambolekwa@gmail.com>
2023-06-08 18:23:29 +0200

    second commit
    updated index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./docs/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "576px",
+      md: "960px",
+      lg: "1440px",
+    },
   },
   plugins: [],
-}
-
+};
